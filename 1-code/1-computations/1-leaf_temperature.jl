b0a12f84476c8360b49cc42ee3912c0d9dedf331
Rémi Vezy <VEZY@users.noreply.github.com>
2023-02-16 10:02:05 +0100

    Update computations of leaf temperature
    Now use a tar.bz2 archive that makes about 24Go

---
+++ 1-code/1-computations/1-leaf_temperature.jl
@@ -0,0 +1,37 @@
+# Aim: compute the leaf temperature from thermal images (FLIR camera)
+# and masks named after the plant, leaf, session and duration of 
+# its validity (sometimes the leaves has moved and we need a new mask).
+# This computation is done in parrallel, and needs access to the thermal 
+# images (JPEG), which takes 60Go of disk space roughly. Please make this 
+# computation only if you have to because it takes time and disk space.
+
+using EcotronAnalysis, DataFrames, CSV, Distributed
+using CodecBzip2, Tar
+
+# Requirements: you need to install exiftool on your machine first, and add it to your PATH.
+# This is needed to read the metadata from the thermal images.
+# You can refer to this page for installation: https://exiftool.org/install.html
+# Or if you use a package manager:
+# `brew install exiftool` # On MacOS
+# `choco install exiftool` # On Windows
+# `sudo apt install libimage-exiftool-perl` # On Linux
+
+# Extract the images from the tar.bz2 file. This can take a while, and the
+# images are 60Go of disk space, so make sure you really need to make this.
+open(Bzip2DecompressorStream, "0-data/0-raw/thermal_camera_images/images.tar.bz2") do io
+    Tar.extract(io, "0-data/0-raw/thermal_camera_images/images")
+end
+
+# Setting up the paths and climate data:
+img_dir = "0-data/0-raw/thermal_camera_images/images/images"
+mask_dir = "0-data/0-raw/thermal_camera_roi_coordinates/coordinates"
+out_dir = "0-data/4-thermal_camera_measurements"
+climate = CSV.read("0-data/1-climate/climate_mic3.csv", DataFrame, dateformat="y-m-d H:M:S")
+
+# Running the computation in parrallel:
+addprocs(exeflags="--project")
+@everywhere using EcotronAnalysis
+compute_jpg_temperature_distributed(img_dir, mask_dir, out_dir, climate)
+rmprocs()
+
+# The results are saved in the out_dir folder, in CSV files named after the images index that were computed in this batch.
